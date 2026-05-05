90f3642afcf5adb1f4b6954a603d3ab2f7c1b374
salh2103y-hue <salh2103y@gmail.com>
2026-05-06 02:56:11 +0300

    Replace express import with GoogleGenerativeAI

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const { GoogleGenerativeAI } = require("@google/generative-ai");const express = require("express");
 const app = express();
 
 app.use(express.json());
